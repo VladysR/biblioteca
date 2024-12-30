f3668d757255db4f45069a2bfb09f4c5ca6cca17
MiJlaXa4Fun <www.wlad.romaniuk13@gmail.com>
2024-12-30 13:03:36 +0100

    proyecto

---
+++ src/main/modelo/DTO_Ejemplar.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "ejemplar")
+public class DTO_Ejemplar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private modelo.DTO_Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public modelo.DTO_Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(modelo.DTO_Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
---
+++ src/main/modelo/DTO_Prestamo.java
@@ -0,0 +1,73 @@
+package modelo;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prestamo")
+public class DTO_Prestamo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private DTO_Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "ejemplar_id", nullable = false)
+    private DTO_Ejemplar ejemplar;
+
+    @Column(name = "fechaInicio", nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(name = "fechaDevolucion")
+    private LocalDate fechaDevolucion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public DTO_Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(DTO_Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public DTO_Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(DTO_Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+}
