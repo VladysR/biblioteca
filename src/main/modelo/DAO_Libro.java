aa7e3c4db1cfcd464a45d2024970d2376291b8ca
MiJlaXa4Fun <www.wlad.romaniuk13@gmail.com>
2024-12-30 13:52:52 +0100

    dao principio

--- src/main/modelo/DAO_Libro.java
+++ src/main/modelo/DAO_Libro.java
@@ -1,5 +1,12 @@
 package modelo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class DAO_Libro {
-    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("Biblioteca");
+    EntityManager em = emf.createEntityManager();
+
+
 }
